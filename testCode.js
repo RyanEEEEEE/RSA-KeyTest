dac92fc2437e6687845229f9e05f181268150c71
SwiftGuard1 <79541415+SwiftGuard1@users.noreply.github.com>
2023-06-03 19:16:21 -0400

    On Stable Version Need to figure out a way to generate keys if keys aren't already stored. If keys are stored, need to figure out how to load them.

--- testCode.js
+++ testCode.js
@@ -1,24 +1,6 @@
-/**
- * Generates a cryptographic key pair asynchronously.
- * @param {AlgorithmIdentifier} alg - The algorithm to be used for key generation.
- * @param {Array} scope - The scope of the key pair.
- * @returns {Promise<CryptoKeyPair>} A promise that resolves to the generated key pair.
- */
-
 function generateKey(alg, scope) {
-  return new Promise(function (resolve) {
-    var genkey = crypto.subtle.generateKey(alg, true, scope);
-    genkey.then(function (pair) {
-      resolve(pair);
-    });
-  });
+  return crypto.subtle.generateKey(alg, true, scope);
 }
-
-/**
- * Converts an ArrayBuffer to a Base64-encoded string.
- * @param {ArrayBuffer} arrayBuffer - The ArrayBuffer to be converted.
- * @returns {string} The Base64-encoded string.
- */
 
 function arrayBufferToBase64String(arrayBuffer) {
   var byteArray = new Uint8Array(arrayBuffer);
@@ -29,12 +11,6 @@ function arrayBufferToBase64String(arrayBuffer) {
   return btoa(byteString);
 }
 
-/**
- * Converts a Base64-encoded string to an ArrayBuffer.
- * @param {string} b64str - The Base64-encoded string.
- * @returns {ArrayBuffer} The converted ArrayBuffer.
- */
-
 function base64StringToArrayBuffer(b64str) {
   var byteStr = atob(b64str);
   var bytes = new Uint8Array(byteStr.length);
@@ -43,12 +19,6 @@ function base64StringToArrayBuffer(b64str) {
   }
   return bytes.buffer;
 }
-
-/**
- * Converts a text string to an ArrayBuffer.
- * @param {string} str - The text string to be converted.
- * @returns {Uint8Array} The converted ArrayBuffer.
- */
 
 function textToArrayBuffer(str) {
   var buf = unescape(encodeURIComponent(str)); // 2 bytes for each char
@@ -59,12 +29,6 @@ function textToArrayBuffer(str) {
   return bufView;
 }
 
-/**
- * Converts an ArrayBuffer to a text string.
- * @param {ArrayBuffer} arrayBuffer - The ArrayBuffer to be converted.
- * @returns {string} The converted text string.
- */
-
 function arrayBufferToText(arrayBuffer) {
   var byteArray = new Uint8Array(arrayBuffer);
   var str = "";
@@ -74,22 +38,9 @@ function arrayBufferToText(arrayBuffer) {
   return str;
 }
 
-/**
- * Converts an ArrayBuffer to a Base64-encoded string.
- * @param {ArrayBuffer} arr - The ArrayBuffer to be converted.
- * @returns {string} The Base64-encoded string.
- */
-
 function arrayBufferToBase64(arr) {
   return btoa(String.fromCharCode.apply(null, new Uint8Array(arr)));
 }
-
-/**
- * Converts binary data to a PEM format.
- * @param {ArrayBuffer} binaryData - The binary data to be converted.
- * @param {string} label - The label for the PEM format.
- * @returns {string} The converted PEM format.
- */
 
 function convertBinaryToPem(binaryData, label) {
   var base64Cert = arrayBufferToBase64String(binaryData);
@@ -108,12 +59,6 @@ function convertBinaryToPem(binaryData, label) {
   return pemCert;
 }
 
-/**
- * Converts a PEM format to binary data.
- * @param {string} pem - The PEM format string.
- * @returns {ArrayBuffer} The binary data.
- */
-
 function convertPemToBinary(pem) {
   var lines = pem.split("\n");
   var encoded = "";
@@ -131,208 +76,168 @@ function convertPemToBinary(pem) {
   return base64StringToArrayBuffer(encoded);
 }
 
-/**
- * Imports a public key from a PEM format.
- * @param {string} pemKey - The PEM format public key.
- * @returns {Promise<CryptoKey>} A promise that resolves to the imported public key.
- */
-
 function importPublicKey(pemKey) {
-  return window.crypto.subtle.importKey(
-    "spki",
-    convertPemToBinary(pemKey),
-    { name: "RSA-OAEP", hash: { name: "SHA-256" } },
-    true,
-    ["encrypt"]
-  );
+  return new Promise(function (resolve) {
+    var importer = crypto.subtle.importKey(
+      "spki",
+      convertPemToBinary(pemKey),
+      { name: "RSA-OAEP", hash: { name: "SHA-256" } },
+      true,
+      ["encrypt"]
+    );
+    importer.then(function (key) {
+      resolve(key);
+    });
+  });
 }
-
-/**
- * Imports a private key from a PEM format.
- * @param {string} pemKey - The PEM format private key.
- * @returns {Promise<CryptoKey>} A promise that resolves to the imported private key.
- */
 
 function importPrivateKey(pemKey) {
-  return window.crypto.subtle.importKey(
-    "pkcs8",
-    convertPemToBinary(pemKey),
-    { name: "RSA-OAEP", hash: { name: "SHA-256" } },
-    true,
-    ["decrypt"]
-  );
-}
-
-/**
- * Exports a public key as a PEM format.
- * @param {CryptoKeyPair} keys - The key pair containing the public key.
- * @returns {Promise<string>} A promise that resolves to the public key in PEM format.
- */
-
-function exportPublicKey(keys) {
   return new Promise(function (resolve) {
-    window.crypto.subtle
-      .exportKey("spki", keys.publicKey)
-      .then(function (spki) {
-        resolve(convertBinaryToPem(spki, "RSA PUBLIC KEY"));
-      });
-  });
-}
-
-/**
- * Exports a private key as a PEM format.
- * @param {CryptoKeyPair} keys - The key pair containing the private key.
- * @returns {Promise<string>} A promise that resolves to the private key in PEM format.
- */
-
-function exportPrivateKey(keys) {
-  return new Promise(function (resolve) {
-    var expK = window.crypto.subtle.exportKey("pkcs8", keys.privateKey);
-    expK.then(function (pkcs8) {
-      resolve(convertBinaryToPem(pkcs8, "RSA PRIVATE KEY"));
+    var importer = crypto.subtle.importKey(
+      "pkcs8",
+      convertPemToBinary(pemKey),
+      { name: "RSA-OAEP", hash: { name: "SHA-256" } },
+      true,
+      ["decrypt"]
+    );
+    importer.then(function (key) {
+      resolve(key);
     });
   });
 }
 
-/**
- * Exports the public and private keys as PEM format.
- * @param {CryptoKeyPair} keys - The key pair containing the public and private keys.
- * @returns {Promise<Object>} A promise that resolves to an object containing the public and private keys in PEM format.
- */
-
-function exportPemKeys(keys) {
-  return new Promise(function (resolve) {
-    exportPublicKey(keys).then(function (pubKey) {
-      exportPrivateKey(keys).then(function (privKey) {
-        resolve({ publicKey: pubKey, privateKey: privKey });
-      });
-    });
-  });
+function exportPublicKey(key) {
+  return crypto.subtle.exportKey("spki", key);
 }
 
-/**
- * Signs data using a cryptographic key.
- * @param {CryptoKey} key - The key used for signing.
- * @param {string} data - The data to be signed.
- * @returns {Promise<ArrayBuffer>} A promise that resolves to the signature as an ArrayBuffer.
- */
+function exportPrivateKey(key) {
+  return crypto.subtle.exportKey("pkcs8", key);
+}
 
 function signData(key, data) {
-  return window.crypto.subtle.sign(signAlgorithm, key, textToArrayBuffer(data));
-}
-
-/**
- * Verifies a signature for the given data using a public key.
- * @param {CryptoKey} pub - The public key used for verification.
- * @param {ArrayBuffer} sig - The signature to be verified.
- * @param {string} data - The data to verify the signature against.
- * @returns {Promise<boolean>} A promise that resolves to true if the signature is valid, false otherwise.
- */
-
-function testVerifySig(pub, sig, data) {
-  return crypto.subtle.verify(signAlgorithm, pub, sig, data);
-}
-
-/**
- * Generates and stores a cryptographic key pair in localStorage.
- * @returns {Promise<Object>} A promise that resolves to an object containing the generated key pair, public key, private key, and the actual CryptoKey pair for future use.
- */
-
-async function generateAndStoreKey() {
-  const storedKeys = localStorage.getItem("keys");
-  let keys = storedKeys ? JSON.parse(storedKeys) : null;
-
-  if (!keys) {
-    // Generate new keys
-    const algorithm = {
-      name: "RSA-OAEP",
-      modulusLength: 2048,
-      publicExponent: new Uint8Array([1, 0, 1]),
-      hash: { name: "SHA-256" }, // Specify the hash algorithm identifier
-    };
-    const keyPair = await generateKey(algorithm, ["encrypt", "decrypt"]);
-
-    // Export the keys as PEM format
-    const publicKey = await exportPublicKey(keyPair.publicKey);
-    const privateKey = await exportPrivateKey(keyPair.privateKey);
-
-    // Store the keys in localStorage
-    keys = {
-      publicKey: convertBinaryToPem(publicKey, "RSA PUBLIC KEY"),
-      privateKey: convertBinaryToPem(privateKey, "RSA PRIVATE KEY"),
-      keyPair: keyPair, // Store the actual CryptoKey pair for future use
-    };
-    localStorage.setItem("keys", JSON.stringify(keys));
-  }
-
-  // Import the keys for encryption and decryption
-  keys.publicKey = await importPublicKey(keys.publicKey);
-  keys.privateKey = await importPrivateKey(keys.privateKey);
-
-  return keys;
-}
-
-/**
- * Encrypts data using a public key.
- * @param {CryptoKey} publicKey - The public key used for encryption.
- * @param {string} data - The data to be encrypted.
- * @returns {Promise<ArrayBuffer>} A promise that resolves to the encrypted data as an ArrayBuffer.
- */
-
-function encryptData(publicKey, data) {
-  return window.crypto.subtle.encrypt(
-    {
-      name: "RSA-OAEP",
-      hash: { name: "SHA-256" },
-    },
-    publicKey,
+  return crypto.subtle.sign(
+    { name: "RSA-PSS", saltLength: 32 },
+    key,
     textToArrayBuffer(data)
   );
 }
 
-/**
- * Decrypts data using a private key.
- * @param {CryptoKey} privateKey - The private key used for decryption.
- * @param {ArrayBuffer} data - The data to be decrypted.
- * @returns {Promise<ArrayBuffer>} A promise that resolves to the decrypted data as an ArrayBuffer.
- */
-
-function decryptData(privateKey, data) {
-  return window.crypto.subtle.decrypt(
-    {
-      name: "RSA-OAEP",
-      hash: { name: "SHA-256" },
-    },
-    privateKey,
-    data
+function verifySignature(key, signature, data) {
+  return crypto.subtle.verify(
+    { name: "RSA-PSS", saltLength: 32 },
+    key,
+    signature,
+    textToArrayBuffer(data)
   );
 }
 
-// Usage example
-generateAndStoreKey().then(function (keys) {
-  console.log("Key Pair:", keys.keyPair);
-  console.log("Public Key:", keys.publicKey);
-  console.log("Private Key:", keys.privateKey);
+function encryptData(key, data) {
+  return crypto.subtle.encrypt(
+    { name: "RSA-OAEP" },
+    key,
+    textToArrayBuffer(data)
+  );
+}
 
-  // Example usage: Encrypt and decrypt a message
-  const message = "Hello, World!";
-  const encryptedMessage = encryptData(keys.publicKey, message);
-  encryptedMessage.then(function (encryptedData) {
-    console.log("Encrypted Data:", encryptedData);
-    console.log("Encrypted Data:", arrayBufferToBase64String(encryptedData));
+function decryptData(key, data) {
+  return crypto.subtle.decrypt({ name: "RSA-OAEP" }, key, data);
+}
 
-    const decryptedMessage = decryptData(keys.privateKey, encryptedData);
-    decryptedMessage.then(function (decryptedData) {
-      const decryptedText = arrayBufferToText(decryptedData);
-      console.log("Decrypted Text:", decryptedText);
+// Generate Key Pair for Encryption/Decryption
+var encryptionKeyPairAlgorithm = {
+  name: "RSA-OAEP",
+  modulusLength: 2048,
+  publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
+  hash: { name: "SHA-256" },
+};
+var encryptionKeyUsageScope = ["encrypt", "decrypt"];
+
+// Generate Key Pair for Signing/Verifying
+var signingKeyPairAlgorithm = {
+  name: "RSA-PSS",
+  modulusLength: 2048,
+  publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
+  hash: { name: "SHA-256" },
+};
+var signingKeyUsageScope = ["sign", "verify"];
+
+generateKey(encryptionKeyPairAlgorithm, encryptionKeyUsageScope).then(function (
+  encryptionKeyPair
+) {
+  var encryptionPublicKey = encryptionKeyPair.publicKey;
+  var encryptionPrivateKey = encryptionKeyPair.privateKey;
+
+  generateKey(signingKeyPairAlgorithm, signingKeyUsageScope).then(function (
+    signingKeyPair
+  ) {
+    var signingPublicKey = signingKeyPair.publicKey;
+    var signingPrivateKey = signingKeyPair.privateKey;
+
+    // Export Public and Private Keys
+    exportPublicKey(encryptionPublicKey).then(function (
+      encryptionPublicKeyExported
+    ) {
+      var exportedEncryptionPublicKey = convertBinaryToPem(
+        encryptionPublicKeyExported,
+        "RSA PUBLIC KEY"
+      );
+      console.log(
+        "Exported Encryption Public Key:",
+        exportedEncryptionPublicKey
+      );
     });
-    const decryptedMessageFromBase64 = decryptData(
-      keys.privateKey,
-      base64StringToArrayBuffer(arrayBufferToBase64String(encryptedData))
-    );
-    decryptedMessageFromBase64.then(function (decryptedData) {
-      const decryptedText = arrayBufferToText(decryptedData);
-      console.log("Decrypted Text:", decryptedText);
+
+    exportPrivateKey(encryptionPrivateKey).then(function (
+      encryptionPrivateKeyExported
+    ) {
+      var exportedEncryptionPrivateKey = convertBinaryToPem(
+        encryptionPrivateKeyExported,
+        "RSA PRIVATE KEY"
+      );
+      console.log(
+        "Exported Encryption Private Key:",
+        exportedEncryptionPrivateKey
+      );
+    });
+
+    exportPublicKey(signingPublicKey).then(function (signingPublicKeyExported) {
+      var exportedSigningPublicKey = convertBinaryToPem(
+        signingPublicKeyExported,
+        "RSA PUBLIC KEY"
+      );
+      console.log("Exported Signing Public Key:", exportedSigningPublicKey);
+    });
+
+    exportPrivateKey(signingPrivateKey).then(function (
+      signingPrivateKeyExported
+    ) {
+      var exportedSigningPrivateKey = convertBinaryToPem(
+        signingPrivateKeyExported,
+        "RSA PRIVATE KEY"
+      );
+      console.log("Exported Signing Private Key:", exportedSigningPrivateKey);
+    });
+
+    // Example Usage: Encrypt and Decrypt
+    var plaintext = "Hello, World!";
+    encryptData(encryptionPublicKey, plaintext).then(function (ciphertext) {
+      console.log("Ciphertext:", arrayBufferToBase64(ciphertext));
+      decryptData(encryptionPrivateKey, ciphertext).then(function (
+        decryptedText
+      ) {
+        console.log("Decrypted Text:", arrayBufferToText(decryptedText));
+      });
+    });
+
+    // Example Usage: Sign and Verify
+    var message = "This is a message to be signed.";
+    signData(signingPrivateKey, message).then(function (signature) {
+      console.log("Signature:", arrayBufferToBase64(signature));
+      verifySignature(signingPublicKey, signature, message).then(function (
+        isValid
+      ) {
+        console.log("Is Valid Signature:", isValid);
+      });
     });
   });
 });
