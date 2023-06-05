3e7676e9d968cf865e1a0a0d8dd700c150699334
SwiftGuard1 <79541415+SwiftGuard1@users.noreply.github.com>
2023-06-04 21:36:06 -0400

    Fixed UI some

--- ui.js
+++ ui.js
@@ -4,10 +4,12 @@ function ocSettings() {
   if (settOpen === true) {
     document.getElementById("openSettings").classList.add("active")
     document.getElementById("settings").style.visibility = "visible"
+    document.getElementById("settingsBehind").style.visibility = "visible"
     settOpen = false
   } else if (settOpen === false) {
     document.getElementById("openSettings").classList.remove("active")
     document.getElementById("settings").style.visibility = "hidden"
+    document.getElementById("settingsBehind").style.visibility = "hidden"
     settOpen = true
   }
 }
